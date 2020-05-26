901192f2970eaa2b20c8d61bf9ec4e6e099fb265
lakemaster <lakelord@jojobi.de>
2020-05-26 13:30:17 +0200

    initial setup

---
+++ src/test/java/com/jojobi/mm/MoneyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojobi.mm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
